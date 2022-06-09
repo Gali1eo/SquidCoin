48784180df5e661a9f11e2545046f07f3ab55c75
Yash Yadav <yashyadav@Yashs-MacBook-Air.local>
2022-06-09 15:57:49 +0530

    first

---
+++ src/components/Transaction.jsx
@@ -0,0 +1,11 @@
+
+
+const Transaction = () => {
+    return(
+        <div className="flex w-full justify-center items-center 2xl:px-20 gradient-bg-transactions">
+            
+        </div>
+    );
+}
+
+export default Transaction
